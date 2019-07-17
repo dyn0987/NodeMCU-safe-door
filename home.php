9578b561f8f45528ada57be70bd363e55d5d3f96
dyn0987 <51043943+dyn0987@users.noreply.github.com>
2019-07-17 16:12:05 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<h2>Welcome</h2>
+<img src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
+<br>
+<form>
+<input class="MyButton" type="button" value="Reset pattern" onclick="window.location.href='update.php';" />
+</form>
+<input class="MyButtons" type="button" value="Back" onclick="window.location.href='index7.html';" />
+</br>
+
+</center>
+
+</body>
+</html>
